ab24f47a7c97084a4ba42e9c40e7ef36a83f53a6
andy.harbert <andy.harbert@oc.edu>
2018-01-23 12:15:29 -0600

    Add project files.

---
+++ Program2/DrinkMachine.cpp
@@ -0,0 +1,12 @@
+//=========================================================
+// header comment block goes here
+//=========================================================
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	return 0;
+}
